aa31791a3f971e22e184acd0cc0c4667abde6b16
Galgaar <mail@isp.fr>
2017-03-28 17:40:48 +0200

    Initial commit

--- app/src/main/java/com/example/rdoison/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rdoison/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rdoison.myapplication;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
